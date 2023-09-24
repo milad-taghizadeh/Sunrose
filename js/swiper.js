0d9e6a7eeaae2685eaffa7c3350d70a2bbde1c4f
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2023-09-24 20:00:41 +0330

    Adding swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,6 @@
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
-register();
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+});
