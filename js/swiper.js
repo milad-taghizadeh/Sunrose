d3e9040a1d403e4370fe2468e95a8e5703d62ff5
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2023-09-27 21:25:18 +0330

    fix:header fixed and dark mode upgraded

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,11 @@
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-    },
-});
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper('.swiper-container', {
+      // Swiper options here
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
